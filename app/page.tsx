8af9c3c99fa921e8b9b7492e756b30647e526c6f
kartatuka <163933179+kartatuka@users.noreply.github.com>
2024-07-13 03:21:25 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home({
     searchParams: { [key: string]: string | string[] | undefined };
 }) {
     const page =
-        (searchParams.page && parseInt(searchParams.page as string)) || 1;
+        (searchParams.page && parseInt(searchParams.page as string)) || 21;
     const per_page =
         (searchParams.per_page && parseInt(searchParams.per_page as string)) ||
         DEFAULT_PER_PAGE;
@@ -18,12 +18,13 @@ export default async function Home({
     const query = (searchParams.q && (searchParams.q as string)) || undefined;
 
     return (
-        <div className="md:my-2"><h1>{SITENAME}</h1><h2>Video Bokep Indo Jepang Barat Terbaru</h2>
+        <div className="md:my-2">
             {query ? (
                 <SearchCardList query={query} banner />
             ) : (
-                <SearchCardList query={SITENAME.split(" ")[1]} />
+                <CardList page={page} per_page={per_page} fld_id={fld_id} />
             )}
+            <h1>{SITENAME}</h1><h2>Video Bokep Indo Jepang Barat Bocil Terbaru</h2>
         </div>
     );
 }
