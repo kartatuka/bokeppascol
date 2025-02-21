a72448c9ddedc3148b5e3e604bd408e1a9cb8b1b
kartatuka <163933179+kartatuka@users.noreply.github.com>
2025-02-21 23:02:25 +0700

    Add files via upload

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -4,22 +4,20 @@ import Image from "next/image";
 import { useState } from "react";
 
 const ImageLoader = ({ src, width, quality }: any) => {
-    return `https://wsrv.nl/?url=${src}&w=320&q=${
+    return `${src}&w=320&q=${
         quality || 80
-    }&fit=cover&output=webp`;
+    }`;
 };
 
 const Thumbnail = ({
-    single_img,
-    splash_img,
+    thumbnail,
     title,
 }: {
-    single_img: string;
-    splash_img: string;
+    thumbnail: string;
     title: string;
 }) => {
     const [imageIndex, setImageIndex] = useState(0);
-    const image = [single_img, splash_img, "https://iili.io/J5ahFSa.png"];
+    const image = [thumbnail, "https://iili.io/J5ahFSa.png"];
 
     const handleImageError = () => {
         if (imageIndex === image.length - 1) return;
